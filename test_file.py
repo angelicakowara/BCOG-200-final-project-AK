206a543d27198651a54136806dfe6d76d58777d1
angelicakowara <129791076+angelicakowara@users.noreply.github.com>
2024-03-26 10:33:22 -0500

    Add files via upload

---
+++ test_file.py
@@ -0,0 +1,16 @@
+Example case + math/logic included
+-user input: 
+  Weight: 75 kg
+  Desired Calories to Burn: 300
+  Speed: 8 km/h
+
+-assume MET value of 8 for 8km/hr
+
+-calculate calories burned per minute
+  Calories per minute=(8 METs×75 kg×3.5)/200
+  Calories per minute=(2100/200) =10.5 calories/minute
+
+-calculate time required 
+time required = 300 calories / (10.5 calories/minute) = 28.6 minutes 
+
+-return time required to the user 
